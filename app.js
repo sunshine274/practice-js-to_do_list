d36e4e3da27b1b517d7532da456336624ba7cfe6
sunshine274 <jingx274@gmail.com>
2020-08-17 10:41:11 +0000

    Initiate the project

---
+++ app.js
@@ -0,0 +1,14 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+function addToDo(toDo) {
+  const text = `<li class="item">
+<i class="co fa fa-circle-thin" job="complete"></i>
+<p class="text">${toDo}</p>
+<i class="de fa fa-trash-o" job="delete"></i>
+</li>`;
+
+  const position = "beforeend";
+}
